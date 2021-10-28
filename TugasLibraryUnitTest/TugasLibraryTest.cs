bcd2952ed82054b1c8a825636c2455e83dd8d81e
RizkyGabS <rizkgabriel20@students.amikom.ac.id>
2021-10-28 21:45:55 +0700

    Add project files.

---
+++ TugasLibraryUnitTest/TugasLibraryTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TugasLibrary;
+using NUnit.Framework;
+
+namespace TugasLibraryUnitTest
+{
+    [TestFixture]
+    public class TugasLibraryTest
+    {
+        private Day day;
+        private User user;
+        [SetUp]
+        public void Init()
+        {
+            day = new Day();
+            user = new User();
+
+        }
+
+        [Test]
+        public void NameOfDayTest()
+        {
+            Assert.AreEqual("Sabtu", day.NameOfDay(6));
+        }
+        [Test]
+        public void IsValidUserTest()
+        {
+            Assert.IsTrue(user.IsValidUser("admin", "admin"));
+        }
+
+    }
+}
